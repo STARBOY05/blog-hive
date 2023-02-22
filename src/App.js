efe589f63fc2c8dfd51b390be53e34a6cb7fe692
Alistair Saldanha <shaanalistair47@gmail.com>
2023-02-22 20:09:58 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { auth } from './firebase-config';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
+
 
 function App() {
+
+
+  const [currUser, setCurrUser] = useState({});
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setCurrUser(currentUser);
+  })
+
+  const logout = async () => {
+    await signOut(auth);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div>
+        <h3>Register User</h3>
+        <input type="text" placeholder="Enter email" onChange={(e) => { setRegisterUser({ ...registerUser, email: e.target.value }) }} />
+        <input type="password" placeholder="Enter password" onChange={(e) => { setRegisterUser({ ...registerUser, password: e.target.value }) }} />
+        <button onClick={register}>Create User</button>
+      </div>
+      <div>
+        <h3>Login User</h3>
+        <input type="text" placeholder="Enter email" onChange={(e) => { setLoginUser({ ...registerUser, email: e.target.value }) }} />
+        <input type="password" placeholder="Enter password" onChange={(e) => { setLoginUser({ ...registerUser, password: e.target.value }) }} />
+        <button onClick={login}>Log-In User</button>
+      </div>
+      <div>
+        <h2>User Logged In: {currUser?.email}</h2>
+      </div>
+      <div>
+        <button onClick={logout}>Logout</button>
+      </div> */}
+    {currUser?.email ? <Register /> : <Login />}
     </div>
   );
 }
---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA7GxZqKUKmNtp_wX_2T97KmbtEATOHumw",
+    authDomain: "blog-hive-05.firebaseapp.com",
+    projectId: "blog-hive-05",
+    storageBucket: "blog-hive-05.appspot.com",
+    messagingSenderId: "134215353611",
+    appId: "1:134215353611:web:16db0bd5ce0e8339cf4e0e",
+    measurementId: "G-NV3HWSN0NL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Firebase Auth
+export const auth = getAuth(app);
