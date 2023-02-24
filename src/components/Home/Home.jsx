9977540366fdec63b60dc476d2f247b4912d4e87
Alistair Saldanha <shaanalistair47@gmail.com>
2023-02-24 19:39:50 +0530

    add and save posts for each user

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,49 +1,44 @@
 import { collection, deleteDoc, doc, getDocs } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
 import { auth, db } from '../../firebase-config';
+import './Home.css';
 
 function Home({ isAuth }) {
 
   const [posts, setPosts] = useState([]);
 
-  const postsCollectionsRef = collection(db, "posts");
+  const postsCollectionsRef = collection(db, "user-posts");
 
   const deletePost = async (id) => {
-    const postDoc = doc(db, "posts", id);
+    const postDoc = doc(db, "user-posts", id);
     await deleteDoc(postDoc);
   };
 
   useEffect(() => {
     const getPosts = async () => {
-      const data = await getDocs(postsCollectionsRef)
+      const data = await getDocs(postsCollectionsRef);
       setPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     }
     getPosts();
   }, [])
 
   return (
-    <div className="homePage">
+    <div className="home-page">
       {posts.map((post) => {
         return (
-          <div className="post">
-            <div className="postHeader">
-              <div className="title">
+          <div className="posts">
+            <div className="post-header">
+              <div className="post-title">
                 <h1> {post.title}</h1>
               </div>
-              <div className="deletePost">
-                {isAuth && post.author.id === auth.currentUser.uid && (
-                  <button
-                    onClick={() => {
-                      deletePost(post.id);
-                    }}
-                  >
-                    {" "}
-                    &#128465;
-                  </button>
-                )}
+              <div className="delete-post">
+                {
+                  isAuth && post.author.id === auth.currentUser.uid && (
+                    <button onClick={() => { deletePost(post.id); }}>🗑️</button>
+                  )}
               </div>
             </div>
-            <div className="postTextContainer"> {post.postText} </div>
+            <div className="post-message"> {post.message} </div>
             <h3>@{post.author.name}</h3>
           </div>
         );
